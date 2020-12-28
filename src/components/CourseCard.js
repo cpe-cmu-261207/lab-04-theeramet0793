4cbd10fe05c1a98774fcfbf237d0b59c391d527d
theeramet0793 <theerametmetha@gmail.com>
2020-12-28 16:50:19 +0700

    อัพเดท

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,9 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return <>
+  <p>{props.name}</p>
+  <p>{props.grd}</p>
+  <p>{props.crd}</p>
+  <button onClick = {() =>props.del(props.name)} > x </button>
+  </>;
 };
