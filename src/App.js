f90a6c607b3af48c6e499d618cc5f490c3e33528
theeramet0793 <theerametmetha@gmail.com>
2020-12-28 16:45:23 +0700

    commit app

--- src/App.js
+++ src/App.js
@@ -6,15 +6,63 @@ function App() {
   const credit = [1, 2, 3];
 
   const [myCourses, setMyCourse] = useState([]);
-  const [inputData, setInputData] = useState({});
+  const [inputData, setInputData] = useState({name:"",grd:"A",crd:"1"});
   const [GPA, setGPA] = useState(4.0);
 
   /**
    * Calculate the GPA of current courses
    * @returns the GPA of current courses
    */
-  function calculateGPA() {
+  function calculateGPA(cc) {
     // TODO
+    var r_gpa = 0
+    var r_cre  = 0 
+    var cal_gpa = 0
+    cc.forEach((item) => {
+      switch(item.grd){
+        case 'A' :
+          r_gpa = 4
+          r_cre += Number(item.crd) 
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'B+' :
+          r_gpa = 3.5
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'B' :
+          r_gpa = 3
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'C+' :
+          r_gpa = 2.5
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'C' :
+          r_gpa = 2
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'D+' :
+          r_gpa = 1.5
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'D' :
+          r_gpa = 1
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'F' :
+          r_gpa = 0
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+      }  
+    });
+    setGPA(cal_gpa / r_cre) 
   }
 
   /**
@@ -25,9 +73,11 @@ function App() {
   function addCourse(event) {
     event.preventDefault();
     // TODO
-
+    console.log(inputData.name)
+    const course = [...myCourses,inputData]
+    setMyCourse(course)
     // recalculate GPA
-    calculateGPA();
+    calculateGPA(course);
   }
 
   /**
@@ -37,6 +87,11 @@ function App() {
    */
   function onDeleteCourse(id) {
     // TODO
+    const course = myCourses.filter(item => {
+      return item.name !== id
+    })
+    setMyCourse(course)
+    calculateGPA(course)
   }
 
   return (
@@ -47,9 +102,34 @@ function App() {
       <div className="h-2/3 md:w-2/4 p-3 rounded-lg mx-auto overflow-auto">
         <h1 className="text-2xl my-3">My courses</h1>
         {/* TODO display courses */}
+        {myCourses.map(item => {
+          return <CourseCard name ={item.name} grd = {item.grd} crd ={item.crd} del={onDeleteCourse} />
+        })}
+        <select onChange = { e => 
+            setInputData({...inputData,crd: e.currentTarget.value}) 
+          }>
+          {credit.map(item => {
+            return <option value={item}>{item}</option>
+          })}
+        </select>     
+        <select onChange = { e => 
+           setInputData({...inputData,grd: e.currentTarget.value}) 
+          }>
+          {grade.map(item => {
+            return <option value={item}>{item}</option>
+          })}
+        </select>    
+        <form onSubmit ={
+          e => addCourse(e)}>
+          <input type="text" onChange = { e => 
+            setInputData({...inputData,name: e.currentTarget.value}) 
+          }/>
+            <button type="submit">+</button>
+       </form>
       </div>
       {/* TODO add course input form */}
       {/* TODO display calculated GPA */}
+      <p>{GPA}</p>
     </div>
   );
 }
